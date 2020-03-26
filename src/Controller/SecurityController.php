7733500a4be5dd463e53de5ec0899737087e3bc9
TimSplinter <timsplinter@live.nl>
2020-03-26 21:51:33 +0100

    login

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,35 +2,55 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\AccountType;
+use App\Form\UserChangeType;
+use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
-    /**
-     * @Route("/login", name="app_login")
-     */
-    public function login(AuthenticationUtils $authenticationUtils): Response
+
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
-
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
-
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        $this->encoder= $encoder;
     }
 
     /**
-     * @Route("/logout", name="app_logout")
+     * @Route("/login", name="app_login")
      */
-    public function logout()
+    public function login(HttpFoundation\Request $request, AuthenticationUtils $utils, AuthorizationCheckerInterface $authChecker)
     {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+        $error = $utils->getLastAuthenticationError();
+        $lastUsername = $utils->getLastUsername();
+
+        if (true === $authChecker->isGranted('ROLE_ADMIN')) {
+            return $this->redirectToRoute('AdminDashboard');
+        }elseif (true === $authChecker->isGranted('ROLE_SUPER-USER')) {
+            return $this->redirectToRoute('dashboard');
+        }elseif (true === $authChecker->isGranted('ROLE_SUPER-USER')) {
+            return $this->redirectToRoute('dashboard');
+        }else {
+            return $this->render('security/login.html.twig', [
+                'error' => $error,
+                'last_username' => $lastUsername,
+                'imgnumber' => rand(1, 6)
+            ]);
+        }
+    }
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logout(){
+
     }
 }
